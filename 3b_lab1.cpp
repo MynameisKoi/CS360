7d092008b10ce30f164c80896cc895c7222b1639
Khoi Duong <89900048+MynameisKoi@users.noreply.github.com>
2022-05-24 19:12:33 -0700

    Create 3b_lab1.cpp

---
+++ 3b_lab1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout, std::endl, std::string;
+
+const string MY_NAME = "Khoi Duong";
+
+int main (void){
+    string name, last_name, first_name;
+
+    last_name = MY_NAME.substr(5,5);
+    first_name = MY_NAME.substr(0,4);
+    name = "Name: " + last_name + ", " + first_name;
+    cout << name << endl;
+
+    return 0;
+}
